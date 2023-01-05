b197aed7ac70c4290efbeede645bb0a919529e18
FadhellNPutra <fadhellnandaputra@gmail.com>
2023-01-05 21:25:37 +0700

    Tugas 9 OOP PHP

---
+++ oop/ape.php
@@ -0,0 +1,12 @@
+<?php 
+    require_once("animal.php");
+
+    class Ape extends Animal{
+        public function yell(){
+            return "Auooo";
+        }
+
+        public $legs = 2;
+    };
+
+?>
